1fb18da60e968c410f26953f407ffa01b306f282
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-08-06 20:10:34 -0500

    moificacion de exito

--- controllers/exit.php
+++ controllers/exit.php
@@ -1,1 +1,17 @@
-exito prueba
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exito</title>
+</head>
+<body>
+    <?php
+    include_once("conexion.php");
+    Connection::getConnection();
+    ?>
+
+    <h1>Exito de prueba</h1>
+   
+</body>
+</html>
